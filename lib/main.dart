67ca8739fe25b2b230ddb5afe6d00d3322f04526
rizaljanuari <rizaljanuari4@gmail.com>
2020-08-07 00:27:11 +0700

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: MainPage(),
+        debugShowCheckedModeBanner: false,
       ),
     );
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Magic 8 Ball App'),
+      ),
+      body: Container(
+        child: Center(
+          child: Image.asset('images/ball1.png'),
+        ),
+      ),
+    );
+  }
+}
