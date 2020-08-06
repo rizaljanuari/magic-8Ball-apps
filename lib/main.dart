7e6fed1f61da369d4e8fb6a84590e895a2984608
rizaljanuari <rizaljanuari4@gmail.com>
2020-08-07 00:34:45 +0700

    add Random function

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: MainPage(),
+        home: Magic(),
         debugShowCheckedModeBanner: false,
       ),
     );
 
-class MainPage extends StatelessWidget {
+class Magic extends StatefulWidget {
+  @override
+  _MagicState createState() => _MagicState();
+}
+
+class _MagicState extends State<Magic> {
+  int answer = 1;
+
+  void randomAnswer() {
+    answer = Random().nextInt(5) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,8 +27,16 @@ class MainPage extends StatelessWidget {
         title: Text('Magic 8 Ball App'),
       ),
       body: Container(
+        color: Colors.blue[100],
         child: Center(
-          child: Image.asset('images/ball1.png'),
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                randomAnswer();
+              });
+            },
+            child: Image.asset('images/ball$answer.png'),
+          ),
         ),
       ),
     );
